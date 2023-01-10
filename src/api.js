021576e4657dd2161b4d1217b8732d1a6cce53fb
Ibrahim Yusuf <iy830755@gmail.com>
2023-01-10 12:09:57 +0000

    oops

--- src/api.js
+++ src/api.js
@@ -3,7 +3,3 @@ const url = "https://nc-games-bib6.onrender.com/api";
 export const getReviews = () => {
     return fetch(url + "/reviews").then((res) => res.json());
 }
-
-export const getSingleReview = (id) => {
-    return fetch(url + `/reviews/${id}`).then((res) => res.json());
-}
