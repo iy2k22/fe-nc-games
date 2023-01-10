fe7a1051a277fb09e9e6896d9fbb0d93ad2892a1
Ibrahim Yusuf <iy830755@gmail.com>
2023-01-10 14:36:15 +0000

    refactor fetch

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 const url = "https://nc-games-bib6.onrender.com/api";
 
-export const getReviews = () => {
-  return fetch(url + "/reviews").then((res) => res.json());
-};
+const fetchAndRetrieve = (endpoint) => {
+  return fetch(url + endpoint).then((res) => res.json());
+}
+
+export const getReviews = () => fetchAndRetrieve("/reviews");
